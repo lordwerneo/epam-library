8119ab54c299ce0075e96611b4a3ba63a67ca486
lordwerneo <lordwerneo@gmail.com>
2021-12-28 22:40:24 +0200

    Minor changes to setup

--- setup.py
+++ setup.py
@@ -3,16 +3,23 @@ from setuptools import setup, find_packages
 
 setup(
 	name='library-app',
-	version='0.1.0',
+	version='1.0.0',
 	description='this package contains samples of project-library code',
 	author='Roman Leontovych',
 	author_email='lordwerneo@gmail.com',
 	url='https://github.com/lordwerneo/project-library/',
-	install_requires=[],
+	install_requires=[
+		'Flask>=2.02',
+		'Flask-Migrate>=3.1.0',
+		'Flask-RESTful>=0.3.9',
+		'Flask-SQLAlchemy>=2.5.1',
+		'Flask-WTF>=1.0.0'
+	],
 	packages=find_packages(exclude=('tests*', 'testing*')),
 	entry_points={
 		'console_scripts': [
 			'test_folder-cli = test_folder.test:main',
 		]
-	}
+	},
+	zip_sage=False,
 )
